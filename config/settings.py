11c46c4a27c585726d9bd1ec4706957a03bbff15
adminnn <geojesi1@mail.ru>
2025-05-21 12:57:59 +0400

    add fanc

--- config/settings.py
+++ config/settings.py
@@ -53,8 +53,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'HW_Django_22',
+        'USER': 'postgres',
+        'PASSWORD': 'Wegodssm1t',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
